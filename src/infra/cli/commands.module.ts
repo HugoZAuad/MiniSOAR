930111f4a68b582087a6999948edec8968f3aafc
HugoZAuadDev <hugo.zeymer@mutualpay.co>
2026-06-06 14:58:15 -0300

    Ajustes para o backend rodar em prod

--- src/infra/cli/commands.module.ts
+++ src/infra/cli/commands.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { CoreModule } from '../../core/core.module';
 import { ScanBatchCommand } from './scan-batch.command';
+import { ScanBatchQuestions } from './scan-batch.questions';
 
 @Module({
   imports: [CoreModule],
-  providers: [ScanBatchCommand],
+  providers: [ScanBatchCommand, ScanBatchQuestions],
 })
 export class CommandsModule {}
