329627f31664074f6b24cd8e62f4642394cf818f
HugoZAuadDev <hugo.zeymer@mutualpay.co>
2026-06-06 11:16:48 -0300

    Ajustes para o backend rodar em prod

--- src/core/core.module.ts
+++ src/core/core.module.ts
@@ -6,22 +6,21 @@ import { PrismaThreatRepository } from '../infra/database/prisma/repositories/pr
 import { LocalFirewallService } from '../infra/providers/firewall/local-firewall.service';
 import { DiscordService } from '../infra/providers/notification/discord.service';
 import { RegisterThreatUseCase } from './application/use-cases/register-threat.use-case';
-
-export const THREAT_REPOSITORY = 'THREAT_REPOSITORY_TOKEN';
-export const THREAT_INTEL_PORT = 'THREAT_INTEL_PORT';
-export const GEO_IP_PORT = 'GEO_IP_PORT';
-export const NOTIFICATION_PORT = 'NOTIFICATION_PORT';
-export const FIREWALL_PORT = 'FIREWALL_PORT';
-export const EVENT_DISPATCHER_PORT = 'EVENT_DISPATCHER_PORT';
+import { EVENT_DISPATCHER_PORT } from './domain/ports/event-dispatcher.port';
+import { FIREWALL_PORT } from './domain/ports/firewall.port';
+import { GEOIP_PORT } from './domain/ports/geoip.port';
+import { NOTIFICATION_PORT } from './domain/ports/notification.port';
+import { THREAT_INTELLIGENCE_PORT } from './domain/ports/threat-intelligence.port';
+import { THREAT_REPOSITORY_TOKEN } from './domain/repositories/threat-repository.token';
 
 @Module({
   providers: [
     RegisterThreatUseCase,
     IndicatorService,
 
-    { provide: THREAT_REPOSITORY, useClass: PrismaThreatRepository },
-    { provide: THREAT_INTEL_PORT, useClass: IndicatorService },
-    { provide: GEO_IP_PORT, useClass: FetchGeoIpAdapter },
+    { provide: THREAT_REPOSITORY_TOKEN, useClass: PrismaThreatRepository },
+    { provide: THREAT_INTELLIGENCE_PORT, useClass: IndicatorService },
+    { provide: GEOIP_PORT, useClass: FetchGeoIpAdapter },
     { provide: NOTIFICATION_PORT, useClass: DiscordService },
     { provide: FIREWALL_PORT, useClass: LocalFirewallService },
     { provide: EVENT_DISPATCHER_PORT, useClass: EventEmitterAdapter },
