e06a682b18d815538f4bfa90ba54f2174207c048
HugoZAuadDev <hugo.zeymer@mutualpay.co>
2026-06-06 04:51:19 -0300

    Ajustes para subir o banco e o back

--- src/infra/cli/commands.module.ts
+++ src/infra/cli/commands.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { CommandRunnerModule } from 'nest-commander';
+import { CoreModule } from '../../core/core.module';
 import { ScanBatchCommand } from './scan-batch.command';
 import { ScanBatchQuestions } from './scan-batch.questions';
 
 @Module({
-  imports: [CommandRunnerModule],
+  imports: [CommandRunnerModule, CoreModule],
   providers: [ScanBatchCommand, ScanBatchQuestions],
 })
 export class CommandsModule {}
